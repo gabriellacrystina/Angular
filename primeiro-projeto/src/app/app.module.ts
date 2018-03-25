a48c4c2eacdab18cff42101c6f0a360fcb2b2650
gabriellacrystina <gabriellacrystina@gmail.com>
2018-03-25 09:02:01 -0300

    Hello world! Criação do primeiro projeto e o primeiro componente

---
+++ primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.componet';
+import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuSegundoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
